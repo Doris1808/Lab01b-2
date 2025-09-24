e1a2215b084d3dbe2e314408f92f3d5160e2cada
Doris1808 <jasonluong1808@gmail.com>
2025-09-24 13:55:26 +0700

    update

---
+++ bai3 lab1a/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace bai3_lab1a.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
